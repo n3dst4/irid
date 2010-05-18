3d1051caa40707d338bb909747f2bd8443e0c951
ned <>
2010-05-18 07:41:43 +0100

    fixed minification script

--- make_minified.py
+++ make_minified.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-from __future__ import with_statement
+
 import urllib2
 import urllib
 
-with open("colour.js") as input, open("colour.min.js", "w") as output:
+with open("colour.js") as input:
     code = input.read()
     
-    minified = urllib2.urlopen("http://closure-compiler.appspot.com/compile",
-        urllib.urlencode({
-            "js_code": code,
-            "compilation_level": "SIMPLE_OPTIMIZATIONS",
-            "output_format": "text",
-            "output_info": "compiled_code"
-        })).read()
+minified = urllib2.urlopen("http://closure-compiler.appspot.com/compile",
+    urllib.urlencode({
+        "js_code": code,
+        "compilation_level": "SIMPLE_OPTIMIZATIONS",
+        "output_format": "text",
+        "output_info": "compiled_code"
+    })).read()
 
-
+with open("colour.min.js", "w") as output:
     output.write(minified)
 
 print("Original:" + str(len(code)) +
